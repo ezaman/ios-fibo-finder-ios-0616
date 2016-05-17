101b4803539c47ca62da9db594ac6475b6456911
ezaman <ehsanmzaman@gmail.com>
2016-05-17 17:05:04 -0400

    fibo done

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,28 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+- (NSArray *)arrayWithFibonacciSequenceToIndex: (NSUInteger) index{
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc]init];
+    
+    for (NSUInteger i = 0; i < index + 1; i++) {
+      if (i == 0) {
+          [sequence addObject:@0]; }
+      else if (i == 1) {
+        [sequence addObject: @1];
+      }
+          else {
+              NSUInteger intA = [sequence [i -2] unsignedIntegerValue];
+              NSUInteger intB = [sequence [i -1] unsignedIntegerValue];
+              NSUInteger nSequence = intA + intB;
+        
+          [sequence addObject: @(nSequence)];
+                }
+            }
+    
+   
+return sequence;
+}
+
 
 @end
